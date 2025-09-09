a21839b0cd0b79761614ca73222b7e796a33ad3d
knull23 <saumil.upadhyay@gmail.com>
2025-09-09 20:21:36 +0530

    a

--- project/backend/app.py
+++ project/backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_cors import cross_origin
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from werkzeug.security import check_password_hash, generate_password_hash
 import os
